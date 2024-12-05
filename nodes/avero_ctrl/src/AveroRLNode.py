865700e7facd33684578145d02a14cb7cb006b83
Dennis Escobar del Rio <descobardelrio@gmail.com>
2024-12-05 14:07:49 +0100

    model_1.zip managed to hover 10 seconds. It was trained with the really rought actuators setpoint update (without using inverse 1° model to update setpoint)

--- nodes/avero_ctrl/src/AveroRLNode.py
+++ nodes/avero_ctrl/src/AveroRLNode.py
@@ -6,7 +6,6 @@ from std_msgs.msg import Float32MultiArray
 from gazebo_msgs.msg import ModelStates
 from omav_msgs.msg import Actuators
 from scipy.spatial.transform import Rotation as R
-import threading
 
 class RLAgentNode:
     def __init__(self, model_path):
@@ -14,7 +13,6 @@ class RLAgentNode:
         rospy.init_node('rl_agent_node', anonymous=True)
         
         self.latest_observation = None
-        self.lock = threading.Lock()  # To avoid race conditions
         self.last_action = np.zeros(9)
         
         self.sub_observations = rospy.Subscriber('/gazebo/model_states', ModelStates, self.observation_callback)
@@ -42,30 +40,28 @@ class RLAgentNode:
             g_bodyframe = orientation.inv().apply(np.array([0, 0, -9.81])) 
             observation = np.concatenate([lin_vel, ang_vel, g_bodyframe, self.last_action])
             
-            with self.lock:
-                self.latest_observation = observation
+            self.latest_observation = observation
 
         except Exception as e:
             rospy.logerr(f"Error in observation callback: {e}")
 
     def publish_observations(self, event):
-        with self.lock:
-            if self.latest_observation is not None:
-                msg_obs = Float32MultiArray()
-                msg_obs.data = self.latest_observation
-                self.pub_observations.publish(msg_obs)
+        if self.latest_observation is not None:
+            msg_obs = Float32MultiArray()
+            msg_obs.data = self.latest_observation
+            self.pub_observations.publish(msg_obs)
 
-                # Publish action
-                action, _ = self.model.predict(self.latest_observation, deterministic=True)
-                self.last_action += action
-                msg_act = Float32MultiArray()
-                msg_act.data = self.last_action
-                self.pub_action.publish(msg_act)
-                action_msg = Actuators()
-                action_msg.angles = [0, 0, 0] + list(self.last_action[3:9])
-                # fanspeed action is normalized in [-1, 1], convert as needed
-                action_msg.angular_velocities = list((self.last_action[0:3] + 1) * 450) + [0, 0, 0, 0, 0, 0]
-                self.pub_command_motorspeed.publish(action_msg)
+            # Publish action
+            action, _ = self.model.predict(self.latest_observation, deterministic=True)
+            self.last_action += action
+            msg_act = Float32MultiArray()
+            msg_act.data = self.last_action
+            self.pub_action.publish(msg_act)
+            action_msg = Actuators()
+            action_msg.angles = [0, 0, 0] + list(self.last_action[3:9])
+            # fanspeed action is normalized in [-1, 1], convert as needed
+            action_msg.angular_velocities = list((self.last_action[0:3] + 1) * 450) + [0, 0, 0, 0, 0, 0]
+            self.pub_command_motorspeed.publish(action_msg)
 
 
 if __name__ == '__main__':
